65150ab701b222bb8cd23a028b0ea2c6c58f5c6a
Алексей Гардер <aleksey_garder@outlook.com>
2018-10-06 13:44:47 +0700

    Add administrator index page

---
+++ AutoshopWebApp/Models/WorkerUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoshopWebApp.Models
+{
+    public class WorkerUser
+    {
+        public int WorkerID { get; set; }
+        public int UserID { get; set; }
+    }
+}
---
+++ AutoshopWebApp/Pages/Admin/AdminNavPages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AutoshopWebApp.Pages.Admin
+{
+    public static class AdminNavPages
+    {
+        public static string WorkersManagment => "WorkersAccuntManagment";
+    }
+}
